6d552e1ea48396284545c2fb929706c40a873801
eckdd <decker.dl@gmail.com>
2018-11-09 08:54:21 -1000

    Update PowerFMC.psm1

--- PowerFMC.psm1
+++ PowerFMC.psm1
@@ -17,11 +17,11 @@ REST account password
     param
     (
         [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
-            [string]$FMCHost,
+            [string]$FMCHost='https://fmcrestapisandbox.cisco.com',
         [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
-            [string]$username,
+            [string]$username='davdecke',
         [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
-            [string]$password
+            [string]$password='jjndAwLh'
     )
 Begin {
 add-type @"
@@ -891,6 +891,7 @@ $body | Add-Member -MemberType NoteProperty -name logBegin        -Value $logBeg
 $body | Add-Member -MemberType NoteProperty -name logEnd          -Value $logEnd
 $body | Add-Member -MemberType NoteProperty -name sendEventsToFMC -Value $SendEventsToFMC
 Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body ($body | ConvertTo-Json -Depth 5)
+($body | ConvertTo-Json -Depth 5)
         }
 End     {}
 }
@@ -1442,6 +1443,8 @@ Name of the rule(s). Wildcards accepted
         [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
             [string]$Domain="$env:FMCDomain",
         [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$false)]
+            [string]$OutFile,
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$false)]
             [switch]$Terse
     )
 Begin   {
@@ -1479,7 +1482,41 @@ while ($pages -gt 1) {
 $response = $items | Where-Object -Property name -Like $RuleName
         }
 End     {
-$response
+if ($OutFile) {
+$fileObject = @()
+$response | foreach {
+$i = New-Object psobject
+$i | Add-Member -MemberType NoteProperty -Name RuleIndex -Value $_.metadata.ruleIndex
+$i | Add-Member -MemberType NoteProperty -Name RuleName  -Value $_.name
+$i | Add-Member -MemberType NoteProperty -Name Action    -Value $_.action
+$srcNet=@()
+if ($_.sourceNetworks.objects)  {$srcNet+= $_.sourceNetworks.objects.name}
+if ($_.sourceNetworks.literals) {$srcNet+= $_.sourceNetworks.literals.value}
+$srcNet = $srcNet -join ",`n"
+$i | Add-Member -MemberType NoteProperty -Name SourceNetwork      -Value $srcNet
+$dstNet=@()
+if ($_.destinationNetworks.objects)  {$dstNet+= $_.destinationNetworks.objects.name}
+if ($_.destinationNetworks.literals) {$dstNet+= $_.destinationNetworks.literals.value}
+$dstNet = $dstNet -join ",`n"
+$i | Add-Member -MemberType NoteProperty -Name DestinationNetwork -Value $dstNet
+$srcPort=@()
+if ($_.sourcePorts.objects)  {$srcPort+= $_.sourcePorts.objects.name}
+if ($_.sourcePorts.literals) {$srcPort+= $MasterProtocolList[[int]$_.sourcePorts.literals.protocol] +'\'+$_.sourcePorts.literals.port}
+$srcPort = $srcPort -join ",`n"
+$i | Add-Member -MemberType NoteProperty -Name SourcePort -Value $srcPort
+$dstPort=@()
+if ($_.destinationPorts.objects)  {$dstPort+= $_.destinationPorts.objects.name}
+if ($_.destinationPorts.literals) {$dstPort+= $MasterProtocolList[[int]$_.destinationPorts.literals.protocol] +'\'+$_.destinationPorts.literals.port}
+$dstPort = $dstPort -join ",`n"
+$i | Add-Member -MemberType NoteProperty -Name DestinationPort -Value $dstPort
+
+$i | Add-Member -MemberType NoteProperty -Name Enabled  -Value $_.enabled
+
+$fileObject += $i
+}
+$fileObject | Export-Csv -Path $OutFile -NoClobber -NoTypeInformation
+ } else {
+$response }
         }
 }
 function Get-FMCZone                {
@@ -1729,7 +1766,6 @@ $headers = @{ "X-auth-access-token" = "$AuthToken" ;'Content-Type' = 'applicatio
 if (!$Enabled)                 {$Enabled                 = $InputObject.enabled}
 if (!$Action)                  {$Action                  = $InputObject.action}
 if (!$urls)                    {$urls                    = $InputObject.urls}
-if (!$users)                   {$users                   = $InputObject.users}
 if (!$vlanTags)                {$vlanTags                = $InputObject.vlanTags}
 if (!$logBegin)                {$logBegin                = $InputObject.logBegin} 
 if (!$logEnd)                  {$logEnd                  = $InputObject.logEnd}
@@ -1938,7 +1974,6 @@ if ($Enabled)                 {$body | Add-Member -MemberType NoteProperty -name
 if ($ruleUUID)                {$body | Add-Member -MemberType NoteProperty -name id                      -Value $ruleUUID}
 if ($Action)                  {$body | Add-Member -MemberType NoteProperty -name action                  -Value $Action}
 if ($urls)                    {$body | Add-Member -MemberType NoteProperty -name urls                    -Value $urls}
-if ($users)                   {$body | Add-Member -MemberType NoteProperty -name users                   -Value $users}
 if ($vlanTags)                {$body | Add-Member -MemberType NoteProperty -name vlanTags                -Value $vlanTags}
 if ($ipsPolicy)               {$body | Add-Member -MemberType NoteProperty -name ipsPolicy               -Value $ipsPolicy }
 if ($fPolicy)                 {$body | Add-Member -MemberType NoteProperty -name filePolicy              -Value $fPolicy }
@@ -2069,3 +2104,146 @@ $body | Add-Member -MemberType NoteProperty -name name         -Value $GroupName
 Invoke-RestMethod -Method Put -Uri $uri -Headers $headers -Body ($body | ConvertTo-Json)
  }
 }
+
+
+$MasterProtocolList = [ordered]@{
+ 0 = 'HOPOPT'
+ 1 = 'ICMP'
+ 2 = 'IGMP'
+ 3 = 'GGP'
+ 4 = 'IPv4'
+ 5 = 'ST'
+ 6 = 'TCP'
+ 7 = 'CBT'
+ 8 = 'EGP'
+ 9 = 'IGP'
+ 10 = 'BBN-RCC-MON'
+ 11 = 'NVP-II'
+ 12 = 'PUP'
+ 13 = 'ARGUS'
+ 14 = 'EMCON'
+ 15 = 'XNET'
+ 16 = 'CHAOS'
+ 17 = 'UDP'
+ 18 = 'MUX'
+ 19 = 'DCN-MEAS'
+ 20 = 'HMP'
+ 21 = 'PRM'
+ 22 = 'XNS-IDP'
+ 23 = 'TRUNK-1'
+ 24 = 'TRUNK-2'
+ 25 = 'LEAF-1'
+ 26 = 'LEAF-2'
+ 27 = 'RDP'
+ 28 = 'IRTP'
+ 29 = 'ISO-TP4'
+ 30 = 'NETBLT'
+ 31 = 'MFE-NSP'
+ 32 = 'MERIT-INP'
+ 33 = 'DCCP'
+ 34 = '3PC'
+ 35 = 'IDPR'
+ 36 = 'XTP'
+ 37 = 'DDP'
+ 38 = 'IDPR-CMTP'
+ 39 = 'TP++'
+ 40 = 'IL'
+ 41 = 'IPv6'
+ 42 = 'SDRP'
+ 43 = 'IPv6-Route'
+ 44 = 'IPv6-Frag'
+ 45 = 'IDRP'
+ 46 = 'RSVP'
+ 47 = 'GRE'
+ 48 = 'DSR'
+ 49 = 'BNA'
+ 50 = 'ESP'
+ 51 = 'AH'
+ 52 = 'I-NLSP'
+ 53 = 'SWIPE'
+ 54 = 'NARP'
+ 55 = 'MOBILE'
+ 56 = 'TLSP'
+ 57 = 'SKIP'
+ 58 = 'IPv6-ICMP'
+ 59 = 'IPv6-NoNxt'
+ 60 = 'IPv6-Opts'
+ 62 = 'CFTP'
+ 64 = 'SAT-EXPAK'
+ 65 = 'KRYPTOLAN'
+ 66 = 'RVD'
+ 67 = 'IPPC'
+ 69 = 'SAT-MON'
+ 70 = 'VISA'
+ 71 = 'IPCV'
+ 72 = 'CPNX'
+ 73 = 'CPHB'
+ 74 = 'WSN'
+ 75 = 'PVP'
+ 76 = 'BR-SAT-MON'
+ 77 = 'SUN-ND'
+ 78 = 'WB-MON'
+ 79 = 'WB-EXPAK'
+ 80 = 'ISO-IP'
+ 81 = 'VMTP'
+ 82 = 'SECURE-VMTP'
+ 83 = 'VINES'
+ 84 = 'TTP'
+ 85 = 'NSFNET-IGP'
+ 86 = 'DGP'
+ 87 = 'TCF'
+ 88 = 'EIGRP'
+ 89 = 'OSPFIGP'
+ 90 = 'Sprite-RPC'
+ 91 = 'LARP'
+ 92 = 'MTP'
+ 93 = 'AX.25'
+ 94 = 'IPIP'
+ 95 = 'MICP'
+ 96 = 'SCC-SP'
+ 97 = 'ETHERIP'
+ 98 = 'ENCAP'
+ 100 = 'GMTP'
+ 101 = 'IFMP'
+ 102 = 'PNNI'
+ 103 = 'PIM'
+ 104 = 'ARIS'
+ 105 = 'SCPS'
+ 106 = 'QNX'
+ 107 = 'A/N'
+ 108 = 'IPComp'
+ 109 = 'SNP'
+ 110 = 'Compaq-Peer'
+ 111 = 'IPX-in-IP'
+ 112 = 'VRRP'
+ 113 = 'PGM'
+ 115 = 'L2TP'
+ 116 = 'DDX'
+ 117 = 'IATP'
+ 118 = 'STP'
+ 119 = 'SRP'
+ 120 = 'UTI'
+ 121 = 'SMP'
+ 122 = 'SM'
+ 123 = 'PTP'
+ 124 = 'ISIS over IPv4'
+ 125 = 'FIRE'
+ 126 = 'CRTP'
+ 127 = 'CRUDP'
+ 128 = 'SSCOPMCE'
+ 129 = 'IPLT'
+ 130 = 'SPS'
+ 131 = 'PIPE'
+ 132 = 'SCTP'
+ 133 = 'FC'
+ 134 = 'RSVP-E2E-IGNORE'
+ 135 = 'Mobility Header'
+ 136 = 'UDPLite'
+ 137 = 'MPLS-in-IP'
+ 138 = 'manet'
+ 139 = 'HIP'
+ 140 = 'Shim6'
+ 141 = 'WESP'
+ 142 = 'ROHC'
+ 255 = 'Reserved'
+ }
